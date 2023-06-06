c37592631a1c6a8002d290f5130873b8ccc91358
sairam <bheemreddy.sairam@eagles.oc.edu>
2023-06-06 18:38:44 -0500

    Access Itineraries

--- app/routes/itinerary.routes.js
+++ app/routes/itinerary.routes.js
@@ -1,20 +1,13 @@
 module.exports = (app) => {
     const Itinerary = require("../controllers/itinerary.controller.js");
-    const Destination = require("../controllers/destination.controller.js");
     const { authenticateRoute } = require("../authentication/authentication");
     var router = require("express").Router();
-    
-    router.get('/itineraries/users/:emailId', Itinerary.findAll);
-    router.post('/itineraries', Itinerary.create);
-    router.get('/itineraries/:itineraryId', Itinerary.findOne);
-    router.put('/itineraries/:itineraryId', Itinerary.update);
-    router.delete('/itineraries/:itineraryId', Itinerary.delete);
 
-
-    app.post('/itineraries/:itineraryId/destinations', Destination.create);
-    app.get('/itineraries/:itineraryId/destinations/:destinationId', Destination.getDetails);
-    app.put('/itineraries/:itineraryId/destinations/:destinationId', Destination.update);
-    app.delete('/itineraries/:itineraryId/destinations/:destinationId', Destination.delete);
+    router.get('/users/:userId/itineraries', Itinerary.getItinerary)
+    router.post('/itineraries', Itinerary.createItinerary)
+    router.get('/itineraries/:itineraryId', Itinerary.findItineraryById)
+    router.put('/itineraries/:itineraryId', Itinerary.updateItinerary)
+    router.delete('/itineraries/:itineraryId', Itinerary.deleteItinerary)
 
   
     app.use("/travelapi", router);
