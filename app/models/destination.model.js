5193e1db68e22e9fe2bb80fe8eb8103b825773c5
Kinnera Chalamalasetty <133031772+kchalamalasetty9@users.noreply.github.com>
2023-06-08 14:58:48 -0500

    Update destination.model.js, automerge messed up model code

--- app/models/destination.model.js
+++ app/models/destination.model.js
@@ -1,9 +1,8 @@
 module.exports = (sequelize, Sequelize) => {
   const Destination = sequelize.define("destination", {
       id: {
-          type: Sequelize.INTEGER,
-          allowNull: false,
-          primaryKey: true,
+        type: Sequelize.STRING,
+        primaryKey: true,
       },
       destination_name: {
         type: Sequelize.STRING,
